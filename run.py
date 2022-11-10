2431adc518f84f2feb5b8601a217c3e3e498d5fe
Tim Balschmiter <38098386+kvlahrobal@users.noreply.github.com>
2022-11-10 05:51:09 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,19 +4,14 @@
 import os 
 import pathlib
 import shutil
-    
-# Directory 
-directory = "models"
+
 runDir = pathlib.Path(__file__).parent.resolve()    
-# Path 
-path = os.path.join(runDir, directory)  
+pathVerkehr = os.path.join(runDir, 'Verkehrszeichen')
 
-os.mkdir(path) 
-print("Directory '% s' created" % path) 
 
-for filename in os.listdir(os.path.join(runDir, 'Verkehrszeichen')):
+for filename in os.listdir(pathVerkehr):
       if filename.endswith('.obj'):
-            print (filename)
+            #print (filename)
             folder = filename.replace('rohling_', '')
             folder = folder.replace('.obj', '')
             path = os.path.join(runDir, 'Verkehrszeichen')
@@ -24,17 +19,25 @@ for filename in os.listdir(os.path.join(runDir, 'Verkehrszeichen')):
             path = os.path.join(path, folder)
             for texturefile in os.listdir(path):
                   if texturefile != 'rohling.jpg':
-                        print (texturefile)
+                        #print (texturefile)
                         
                         dstObj = texturefile.replace('.jpg','.obj')
-                        print (dstObj)
+                        #print (dstObj)
                         dstMtl = texturefile.replace('.jpg','.mtl')
-                        print (dstMtl)
+                        #print (dstMtl)
                         mtlFile = filename.replace('.obj','.mtl')
-                        print(mtlFile)
-                        tmpFolder = path = os.path.join(runDir,'tmp')
-                        print(tmpFolder)
+                        #print(mtlFile)
+                        tmpFolder = os.path.join(runDir,'tmp')
+                        #print(tmpFolder)
                         
-                        #shutil.copyfile(filename, os.path.join(tmpFolder,dstObj))
-                        #shutil.copyfile(filename, os.path.join(tmpFolder,dstMtl))
+                        os.mkdir(tmpFolder) 
+                        #print("Directory '% s' created" % path) 
+                        srcObj = os.path.join(pathVerkehr,filename)
+                        srcMtl = os.path.join(pathVerkehr,mtlFile)
+                        shutil.copyfile(srcObj, os.path.join(tmpFolder,dstObj))
+                        shutil.copyfile(srcMtl, os.path.join(tmpFolder,dstMtl))
+                        
+for filename in os.listdir(os.path.join(runDir,'tmp')):
+      if filename.endswith('.obj'):
+            print (filename)                        
             
