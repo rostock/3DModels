fa754030185a9b68112baf76a4eefd7f7d33e5d7
Hagen Schoenkaese <105041704+kvlahrosch@users.noreply.github.com>
2022-11-10 10:48:58 +0100

    Update makeSigns.py

--- makeSigns.py
+++ makeSigns.py
@@ -7,9 +7,19 @@ pathVerkehr = os.path.join(runDir, 'Verkehrszeichen')
 
 for filename in os.listdir(pathVerkehr):
     if filename.endswith('.obj'):
+        # Directory
+        directory = "obj_files"
+  
+        # Parent Directory path
+        parent_dir = pathVerkehr
+        # Path
+        path = os.path.join(parent_dir, directory)
+        # mkdir
+        os.mkdir(path)
+        
         base, extension = os.path.splitext(filename)
         oldname = str(pathVerkehr) + "/" + filename
-        newname = str(pathVerkehr) + "/" + base + "_new" + extension
+        newname = str(path) + "/" + base + "_new" + extension
         print(oldname)
         print(newname)
         shutil.copy(oldname, newname)
