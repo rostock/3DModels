dc36375534e19a247e6c78348185ba2ae57cc978
Tim Balschmiter <38098386+kvlahrobal@users.noreply.github.com>
2022-11-10 11:54:20 +0100

    Update run.py

--- run.py
+++ run.py
@@ -42,7 +42,26 @@ for filename in os.listdir(pathVerkehr):
                         shutil.copy2(srcObj, os.path.join(tmpFolder,dstObj))
                         shutil.copy2(srcMtl, os.path.join(tmpFolder,dstMtl))
                         
-for filename in os.listdir(os.path.join(runDir,'tmp')):
-      if filename.endswith('.obj'):
-            print (filename)                        
+tmpPath = os.path.join(runDir,'tmp')
+oldText = "rohling.jpg"
+for filename in os.listdir(tmpPath):
+      if filename.endswith('.mtl'):
+            newText = filename.replace('mtl','jpg')
+            print (filename)    
+            #read mtl file
+            fin = open(os.path.join(tmpPath, filename), "rt")
+            #read file contents to string
+            data = fin.read()
+            #replace all occurrences of the required string
+            data = data.replace(oldText, newText)
+            #close the input file
+            fin.close()
+            #open the input file in write mode
+            fin = open("data.txt", "wt")
+            #overrite the input file with the resulting data
+            fin.write(data)
+            #close the file
+            fin.close()
+            
+            
             
