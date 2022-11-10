1862c058593b75bb8a61e4d4c0a1149be6ad6a8b
Hagen Schoenkaese <105041704+kvlahrosch@users.noreply.github.com>
2022-11-10 09:17:51 +0100

    Update makeSigns.py

--- makeSigns.py
+++ makeSigns.py
@@ -2,10 +2,14 @@ import os
 import pathlib
 import shutil
 
-runDir = pathlib.Path(__file__).parent.resolve()    
-pathVerkehr = os.path.join(runDir, 'Verkehrszeichen')
+runDir = pathlib.Path(__file__).parent.resolve()
+pathVerkehr = os.path.join(runDir, 'OBJ')
 
 for filename in os.listdir(pathVerkehr):
-    newname = filename + "new"
     if filename.endswith('.obj'):
-      shutil.copy(filename, newname)
+        base, extension = os.path.splitext(filename)
+        oldname = str(pathVerkehr) + "\\" + filename
+        newname = str(pathVerkehr) + "\\" + base + "_new" + extension
+        print(oldname)
+        print(newname)
+        shutil.copy(oldname, newname)
