5a85e998424f59af7ebe8c1809e251282c7d85dc
Hagen Schoenkaese <hagen.schoenkaese@rostock.de>
2022-11-15 13:09:06 +0100

    Update exportObjFromBlend.py

--- exportObjFromBlend.py
+++ exportObjFromBlend.py
@@ -11,4 +11,4 @@ for blendfile in os.listdir(blendPath):
       bpy.ops.wm.open_mainfile(filepath=currentFile)
       blendfileName = blendfile.replace('.blend', '.obj')
       exportPath = os.path.join(pathVerkehr,blendfileName)
-      bpy.ops.export_scene.obj(filepath=exportPath, axis_forward='-Y', axis_up='Z', use_materials=True, path_mode='RELATIV')
+      bpy.ops.export_scene.obj(filepath=exportPath, axis_forward='-Y', axis_up='Z', use_materials=True, path_mode='RELATIVE')
