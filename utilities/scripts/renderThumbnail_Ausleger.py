f9ecfc363e763fa0232de719d6319f0181d5c043
Hagen Schoenkaese <hagen.schoenkaese@rostock.de>
2023-04-19 16:53:43 +0200

    added action for ausleger

--- utilities/scripts/renderThumbnail_Ausleger.py
+++ utilities/scripts/renderThumbnail_Ausleger.py
@@ -6,7 +6,7 @@ import git
 runDir = git.Repo(".",search_parent_directories=True)
 runDir = runDir.git.rev_parse("--show-toplevel")
 # path to blendfiles   
-pathBlend = os.path.join(runDir, 'Beleuchtung/Lampen/blender')
+pathBlend = os.path.join(runDir, 'Beleuchtung/Ausleger/blender')
 
 for blendfile in os.listdir(pathBlend):
     currentFile = os.path.join(pathBlend,blendfile)
@@ -15,7 +15,7 @@ for blendfile in os.listdir(pathBlend):
     # create name for the image
     blendfileName = blendfile.replace('.blend', '.jpg')
     # create export path
-    exportPath = os.path.join(runDir, 'Thumbnails/Lampen')
+    exportPath = os.path.join(runDir, 'Thumbnails/Ausleger')
     exportPathPlusName = os.path.join(exportPath,blendfileName)
     # set export format to jpeg
     bpy.context.scene.render.image_settings.file_format='JPEG'
