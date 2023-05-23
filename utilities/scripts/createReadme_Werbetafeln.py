d7aed972d463cbcbfa6fff9e0cb3abad63707e6e
Dominik Neu <128499582+kvlahroneu@users.noreply.github.com>
2023-05-23 12:14:34 +0200

    Update createReadme_Werbetafeln.py

--- utilities/scripts/createReadme_Werbetafeln.py
+++ utilities/scripts/createReadme_Werbetafeln.py
@@ -6,16 +6,16 @@ runDir = git.Repo(".",search_parent_directories=True)
 runDir = runDir.git.rev_parse("--show-toplevel")     
 pathThumb = os.path.join(runDir, 'Thumbnails/Werbetafeln')
 listOfModels = os.listdir(pathThumb)
-models ='## Modelle \n | Modellname | Preview | \n | --- | --- |\n'
+models ='## Modelle \n | Modellname | Preview | 3D-Modell | \n | --- | --- | --- |\n'
 for item in listOfModels:
     image = os.path.join("../Thumbnails/Werbetafeln",item)
     item = item.replace('.jpg','')
-    string = "| "+item+" |![Image]("+image +")| \n"
+    string = "| "+item+" |![Image]("+image +")| [Link zu Online 3D Viewer] (https://3dviewer.net/embed.html#model=https://github.com/rostock/3DModels/blob/dev/GLBFiles/Werbetafeln/ "+item+".glb$camera=0,0,0$cameramode=perspective$envsettings=fishermans_bastion,on$backgroundcolor=200,200,200,255$defaultcolor=200,200,200$edgesettings=off,0,0,0,20|)  |\n"
     models += string
 
 text = """# Werbetafelnn
 ## Allgemein
-Dieses Verzeichnis enthält Modelle von Werbetafeln.
+Dieses Verzeichnis enthält Modelle von Werbetafeln und Litfaßsäulen.
 
 ## Grundlage
 Als Grundlage für die zur Verfügung gestellten Modelle dienen **Fotos** und **Produktskizzen/-maße** der jeweiligen Realweltobjekte. \n"""
