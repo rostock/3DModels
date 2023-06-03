8d54bcfeddafdcafe46d7b172cd1bd2d3f63d0b6
Dominik Neu <128499582+kvlahroneu@users.noreply.github.com>
2023-06-03 02:56:46 +0200

    Update exportOBJ_Masten.py

--- utilities/scripts/exportOBJ_Masten.py
+++ utilities/scripts/exportOBJ_Masten.py
@@ -15,7 +15,7 @@ for blendfile in os.listdir(pathBlend):
       bpy.ops.wm.open_mainfile(filepath=currentFile)
       blendfileName = blendfile.replace('.blend', '.obj')
       exportPath = os.path.join(pathOBJ,blendfileName)
-      bpy.ops.export_scene.obj(filepath=exportPath, axis_forward='-Y', axis_up='Z', use_materials=True, path_mode='RELATIVE')
+      bpy.ops.export_scene.obj(filepath=exportPath, axis_forward='-Y', axis_up='Z', use_materials=True, use_triangles=True, path_mode='RELATIVE')
 
 textureFolder = os.path.join(runDir,'Beleuchtung/Masten/textures')
 dest = os.path.join(pathOBJ,'textures/')
