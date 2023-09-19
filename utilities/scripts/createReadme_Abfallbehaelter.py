0a8b3f4abcb4651b9156e3ee8f031bedecdabfdf
Hagen Schoenkaese <hagen.schoenkaese@rostock.de>
2023-09-19 16:14:07 +0200

    Kameras angepasst, Pfad aktualisiert

--- utilities/scripts/createReadme_Abfallbehaelter.py
+++ utilities/scripts/createReadme_Abfallbehaelter.py
@@ -8,7 +8,7 @@ pathThumb = os.path.join(runDir, 'Thumbnails/Abfallbehaelter')
 listOfModels = os.listdir(pathThumb)
 models ='## Modelle \n | Modellname | Preview | \n | --- | --- | \n'
 for item in listOfModels:
-    image = os.path.join("../../Thumbnails/Abfallbehaelter",item)
+    image = os.path.join("../Thumbnails/Abfallbehaelter",item)
     item = item.replace('.jpg','')
     string = "| "+item+" |![Image]("+image +")| \n"
     models += string
