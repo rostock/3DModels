af3f2541944db4fb0b5ca0e469d463c9b266c29d
Hagen Schoenkaese <hagen.schoenkaese@rostock.de>
2023-10-04 13:16:54 +0200

    Update retextureOBJ_Verkehrszeichen.py

--- utilities/scripts/retextureOBJ_Verkehrszeichen.py
+++ utilities/scripts/retextureOBJ_Verkehrszeichen.py
@@ -76,8 +76,8 @@ for filename in os.listdir(pathOBJ):
             data = fin.read()
             #replace all occurrences of the required string
             data = data.replace(oldText, newText)
-
             data = data.replace('..\\\\',' ')
+            data = data.replace('../','../../Verkehrszeichen/')
             #close the input file
             fin.close()
             #open the input file in write mode
