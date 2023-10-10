16de1fc13f59957cd72186d61d2f9e01deef3eeb
Hagen Schoenkaese <hagen.schoenkaese@rostock.de>
2023-10-10 17:06:33 +0200

    test for path fix #1

--- utilities/scripts/exportOBJ_Abfallbehaelter.py
+++ utilities/scripts/exportOBJ_Abfallbehaelter.py
@@ -15,7 +15,7 @@ for blendfile in os.listdir(pathBlend):
       bpy.ops.wm.open_mainfile(filepath=currentFile)
       blendfileName = blendfile.replace('.blend', '.obj')
       exportPath = os.path.join(pathOBJ,blendfileName)
-      bpy.ops.export_scene.obj(filepath=exportPath, axis_forward='-Y', axis_up='Z', use_materials=True, path_mode='AUTO')
+      bpy.ops.export_scene.obj(filepath=exportPath, axis_forward='-Y', axis_up='Z', use_materials=True, path_mode='RELATIVE')
 
 textureFolder = os.path.join(runDir,'Abfallbehaelter/textures')
 dest = os.path.join(pathOBJ,'textures/')
@@ -23,3 +23,21 @@ os.makedirs(dest, exist_ok=True)
 for file in os.listdir(textureFolder):
     fileDir = os.path.join(textureFolder,file)
     shutil.copy2(fileDir, dest)
+
+# replace texture reference in .mtl                     
+for filename in os.listdir(pathOBJ):
+      if filename.endswith('.mtl'):
+            #read mtl file
+            fin = open(os.path.join(pathOBJ, filename), "rt")
+            #read file contents to string
+            data = fin.read()
+            #replace all occurrences of the required string
+            data = data.replace('../../','../')
+            #close the input file
+            fin.close()
+            #open the input file in write mode
+            fin = open(os.path.join(pathOBJ, filename), "wt")
+            #overrite the input file with the resulting data
+            fin.write(data)
+            #close the file
+            fin.close()
